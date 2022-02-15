a2c96db95388643d724a12dec3d838a66ab31cde
BaffledDevelopment <cirnotan9@gmail.com>
2022-02-15 18:05:12 +0300

    Removed splash-screen, add bare-bones for auth, app-drawer for main screen.

---
+++ lib/ui/widgets/application_drawer.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class AppDrawer extends StatelessWidget {
+  const AppDrawer(
+      BuildContext context, {
+        Key? key,
+      }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Image.asset(
+              'assets/icons/wallet.png',
+              width: 100,
+              height: 100,
+              alignment: Alignment.centerLeft,
+            ),
+            decoration: const BoxDecoration(
+              color: Color.fromARGB(255, 255, 241, 159),
+            ),
+          ),
+          ListTile(
+            title: const Text('Chart'),
+            leading: const Icon(Icons.pie_chart),
+            onTap: () {
+              Navigator.of(context).pushNamed("chart");
+            },
+          ),
+          const Divider(
+            thickness: 1,
+          )
+        ],
+      ),
+    );
+  }
+}

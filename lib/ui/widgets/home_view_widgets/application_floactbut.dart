4b24e2a132efb9e173e488bf19c2df1b83c65fcc
BaffledDevelopment <cirnotan9@gmail.com>
2022-02-16 00:16:07 +0300

    fix float button redirect

--- lib/ui/widgets/home_view_widgets/application_floactbut.dart
+++ lib/ui/widgets/home_view_widgets/application_floactbut.dart
@@ -2,14 +2,18 @@ import 'package:flutter/material.dart';
 
 
 class ApplicationFloActBut extends StatelessWidget {
-  const ApplicationFloActBut({
-    Key? key,
-  }) : super(key: key);
+
+  final Function closeMonthPicker;
+
+  const ApplicationFloActBut(this.closeMonthPicker);
 
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
-      onPressed: () {},
+      onPressed: () {
+        closeMonthPicker();
+        Navigator.of(context).pushNamed("new_transaction");
+      },
       child: const Icon(Icons.add, color: Colors.black38),
       backgroundColor: const Color.fromARGB(255, 255, 241, 159),
     );

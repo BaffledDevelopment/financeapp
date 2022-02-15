a145ee2620a8f2e4a37f01f52b5cd7e5e03f5949
BaffledDevelopment <cirnotan9@gmail.com>
2022-02-15 22:37:14 +0300

    Null. Safety.

---
+++ lib/ui/widgets/home_view_widgets/app_bar.dart
@@ -0,0 +1,39 @@
+import 'package:finances/viewmodels/home_model.dart';
+import 'package:flutter/material.dart';
+
+class MyAppBar extends StatelessWidget {
+  final HomeModel? model;
+  final String? title;
+  const MyAppBar({Key? key, this.model, this.title})
+      : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        model?.titleClicked();
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              title!,
+              style: TextStyle(fontWeight: FontWeight.w500, fontSize: 20),
+            ),
+            model!.isCollapsed
+                ? const Icon(
+              Icons.arrow_drop_down,
+            )
+                : const Icon(
+              Icons.arrow_drop_up,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

311792cf47f55c5fb02f85b401d2712e8aa495ab
BaffledDevelopment <cirnotan9@gmail.com>
2022-02-16 13:52:20 +0300

    create new transaction model&view, todo add button

--- lib/ui/views/create_new_transaction_view.dart
+++ lib/ui/views/create_new_transaction_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:finances/models/category.dart';
+import 'package:finances/viewmodels/create_new_transaction_model.dart';
 import 'package:finances/ui/views/base_view.dart';
 
 class CreateNewTransactionView extends StatefulWidget {
@@ -9,13 +10,93 @@ class CreateNewTransactionView extends StatefulWidget {
 
   CreateNewTransactionView(this.category, this.selectedCategory);
 
-
-
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
+  _CreateNewTransactionViewState createState() =>
+      _CreateNewTransactionViewState();
+
+
+}
+
+class _CreateNewTransactionViewState extends State<CreateNewTransactionView>{
+  String _value = '';
+
+  Future _selectDate() async {
+    DateTime? picked = await showDatePicker(
+        context: context,
+        initialDate: new DateTime.now(),
+        firstDate: DateTime(2020),
+        lastDate: DateTime.now());
+    if (picked != null) setState(() => _value = picked.toString());
+
+    print(picked?.day);
+    print(picked?.month);
   }
 
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController memoController = TextEditingController();
+    TextEditingController amountController = TextEditingController();
+
+    return BaseView<CreateNewTransactionModel>(
+      builder: (context, model, child) => Scaffold(
+        appBar: AppBar(
+          title:
+          widget.selectedCategory == 1 ? Text('Income') : Text('Expense'),
+        ),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: <Widget>[
+                buildTextField(memoController, 'Note:',
+                    "Enter a note for your transaction", Icons.edit, false),
+                Container(height: 20.0),
+                buildTextField(
+                    amountController,
+                    'Amount:',
+                    "Enter a the amount for the transaction",
+                    Icons.attach_money,
+                    true),
+                RaisedButton(onPressed: () async {
+                  await _selectDate();
+                })
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  TextFormField buildTextField(TextEditingController controller, String text,
+      String helperText, IconData icon, isNumeric) {
+    return TextFormField(
+      cursorColor: Colors.black,
+      maxLength: isNumeric ? 10 : 40,
+      keyboardType: isNumeric ? TextInputType.number : TextInputType.text,
+      controller: controller,
+      decoration: InputDecoration(
+        border: const OutlineInputBorder(),
+        icon: Icon(
+          icon,
+          color: Colors.black,
+        ),
+        labelText: text,
+        suffixIcon: InkWell(
+          onTap: () {
+            controller.clear();
+          },
+          child: const Icon(
+            Icons.clear,
+            color: Colors.black,
+          ),
+        ),
+        labelStyle: const TextStyle(
+          color: Color(0xffff000000),
+        ),
+        helperText: helperText,
+      ),
+    );
+  }
 
 }
--- lib/viewmodels/create_new_transaction_model.dart
+++ lib/viewmodels/create_new_transaction_model.dart
@@ -1,6 +1,98 @@
 import 'package:finances/viewmodels/base_model.dart';
+import 'package:flutter/material.dart';
+import 'package:finances/database/databaseimpl.dart';
+import 'package:finances/services/database_service.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+import '../locator.dart';
 
 class CreateNewTransactionModel extends BaseModel {
 
+  TextEditingController memoController = TextEditingController();
+  TextEditingController amountController = TextEditingController();
 
+  final DataBaseService _databaseService =
+  locator<DataBaseService>();
+
+  List months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec'
+  ];
+
+  late String selectedDay;
+  late String selectedMonth;
+  DateTime selectedDate = DateTime.now();
+  late String type;
+  late int categoryIndex;
+
+  Future selectDate(context) async {
+    // hide the keyboard
+    unFocusFromTheTextField(context);
+
+    DateTime? picked = await showDatePicker(
+        context: context,
+        initialDate: selectedDate,
+        firstDate: DateTime(2020),
+        lastDate: DateTime.now());
+
+    if (picked != null) {
+      selectedMonth = months[picked.month - 1];
+      selectedDay = picked.day.toString();
+      selectedDate = picked;
+
+      notifyListeners();
+    }
+  }
+
+  void init(int selectedCategory, int index) {
+    // starting values are current day&month
+    selectedMonth = months[DateTime.now().month - 1];
+    selectedDay = DateTime.now().day.toString();
+    type = (selectedCategory == 1) ? 'income' : 'expense';
+    categoryIndex = index;
+  }
+
+  void unFocusFromTheTextField(context) {
+    FocusScope.of(context).requestFocus(FocusNode());
+  }
+
+  String getSelectedDate() {
+    if (int.parse(selectedDay) == DateTime.now().day &&
+        DateTime.now().month == months.indexOf(selectedMonth) + 1) {
+      return 'Today ' + selectedMonth + ',' + selectedDay;
+    } else {
+      return selectedMonth + ',' + selectedDay;
+    }
+  }
+
+  addTransaction(context) async {
+    String amount = amountController.text;
+
+
+
+    Transaction newTransaction = Transaction(
+        type: type,
+        day: selectedDay,
+        month: selectedMonth,
+        memo: memoController.text,
+        amount: int.parse(amount),
+        categoryindex: categoryIndex);
+    // insert it!
+    await _databaseService.insertTransaction(newTransaction);
+
+
+    // return to the home
+    Navigator.of(context)
+        .pushNamedAndRemoveUntil('home', (Route<dynamic> route) => false);
+  }
 }

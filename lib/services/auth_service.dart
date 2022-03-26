3be701ed117105f35c263f02d5880734d2284350
BaffledDevelopment <cirnotan9@gmail.com>
2022-03-26 11:08:27 +0300

    Implement authentification service

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,3 +1,38 @@
-class AuthenticationService {}
+import 'package:firebase_auth/firebase_auth.dart';
 
-// todo Implement auth, probably will use placeholder firebase auth this is hard
+class AuthenticationService {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  Future loginWithEmail({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      var user = await _firebaseAuth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return user != null;
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future signUpWithEmail({
+    required String email,
+    required String password,
+    required String name,
+  }) async {
+    try {
+      var authResult = await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      return authResult.user != null;
+
+    } catch (e) {
+      print(e);
+    }
+  }
+}

acd2cf1c1753341eacea0da9a1035fc5bdbde190
BaffledDevelopment <cirnotan9@gmail.com>
2022-03-26 11:13:04 +0300

    add registration (sign_up) view and viewmodel

---
+++ lib/ui/views/register_view.dart
@@ -0,0 +1,113 @@
+import 'package:finances/ui/views/auth_view.dart';
+import 'package:finances/ui/views/base_view.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:finances/viewmodels/register_view_model.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:finances/locator.dart';
+
+import '../rounded_field.dart';
+import '../rounded_button.dart';
+import '../rounded_input_field.dart';
+
+class SignupPage extends StatelessWidget {
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final passwordConfirmController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return BaseView<RegisterViewModel>(
+      // onModelReady: (model) => model.init(),
+      builder: (context, model, widget) => Scaffold(
+        // backgroundColor: Color(0xFFF0F0F0),
+        appBar: AppBar(
+          backgroundColor: Colors.cyan,
+          elevation: 0,
+          leading: GestureDetector(
+            onTap: () {
+              Navigator.pop(context!);
+            },
+            child: Icon(
+              Icons.arrow_back,
+              size: 30,
+              color: Colors.black,
+            ),
+          ),
+        ),
+        body: SafeArea(
+          child: Container(
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        "Start saving money!",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 30),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Create an account to get all features',
+                        style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          fontSize: MediaQuery.of(context!).size.width * 0.03,
+                          color: Color(0xFFA1A1A1),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: RoundedInputField(
+                        hintText: "Name",
+                        controller: nameController,
+                        onChanged: (name) {},
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: RoundedInputField(
+                        hintText: "Email",
+                        controller: emailController,
+                        onChanged: (email) {},
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: RoundedPasswordField(
+                        controller: passwordController,
+                        onChanged: (password) {},
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: RoundedPasswordField(
+                        controller: passwordConfirmController,
+                        onChanged: (email) {},
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: RoundedButton(press: () {  }, text: '',),
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.width * 0.1,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/viewmodels/register_view_model.dart
@@ -0,0 +1,61 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:finances/locator.dart';
+import 'package:finances/services/auth_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:finances/ui/router.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+import '../enum_viewstate.dart';
+import '../ui/views/home_view.dart';
+import 'base_model.dart';
+
+class RegisterViewModel extends BaseModel {
+  final AuthenticationService _authenticationService =
+  locator<AuthenticationService>();
+
+  Future signUp({
+    required String email,
+    required String password,
+    required String name,
+    required BuildContext context,
+  }) async {
+    setState(ViewState.Busy);
+
+    var result = await _authenticationService.signUpWithEmail(
+        email: email, password: password, name: name);
+
+    setState(ViewState.Idle);
+
+    if (result is bool) {
+      // TODO: navigation inside model IS ABSOLUTE HARAM
+
+      if (result) {
+        Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(builder: (BuildContext context) => HomeView()),
+                (Route<dynamic> route) => false);
+
+        final user = FirebaseAuth.instance.currentUser!;
+
+        FirebaseFirestore.instance
+            .collection('users')
+            .doc(user.uid)
+            .set({
+          'uid': user.uid,
+          'name': name,
+          'email': email,
+          'hashedPassword': password,
+        })
+            .then((value) => print("User added!"))
+            .catchError((e) => print("Failed to add user"))
+        as CollectionReference<Object?>;
+      } else {
+        Fluttertoast.showToast(
+            msg: "General sign up failure. Please try again later",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.BOTTOM);
+      }
+    }
+  }
+}

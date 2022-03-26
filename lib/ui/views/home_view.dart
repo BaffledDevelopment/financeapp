08b556332d0eb8fac063170f94460f320d8f445c
BaffledDevelopment <cirnotan9@gmail.com>
2022-03-26 11:55:49 +0300

    button for debugging firebase data

--- lib/ui/views/home_view.dart
+++ lib/ui/views/home_view.dart
@@ -6,6 +6,7 @@ import 'package:finances/ui/widgets/home_view_widgets/monthYearInterface.dart';
 import 'package:finances/ui/widgets/home_view_widgets/no_transactions_widget.dart';
 import 'package:finances/ui/widgets/home_view_widgets/summary.dart';
 import 'package:finances/ui/widgets/home_view_widgets/transactions_listwiew_widget.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:flutter/material.dart';
 import 'package:finances/viewmodels/home_model.dart';
@@ -14,36 +15,54 @@ import 'package:finances/ui/views/base_view.dart';
 import 'package:finances/ui/widgets/home_view_widgets/application_floactbut.dart';
 
 import '../../enum_viewstate.dart';
+import '../../services/firebase_database_service.dart';
+import '../rounded_button.dart';
 
 class HomeView extends StatelessWidget {
 
+
+
   @override
   Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser!;
+    final fdb_service = FirebaseDatabaseService();
     return BaseView<HomeModel>(
       onModelReady: (model) async => await model.init(),
       builder: (context, model, child) => Scaffold(
         appBar: buildAppBar(model!.appBarTitle, model),
         drawer: AppDrawer(context!),
-        floatingActionButton: ApplicationFloActBut(model.closeMonthPicker), // push to new transaction
+        floatingActionButton: ApplicationFloActBut(model.closeMonthPicker),
+        // push to new transaction
         body: model.state == ViewState.Busy
             ? const Center(child: CircularProgressIndicator())
             : Stack(
-          children: <Widget>[
-            Column(
-              children: <Widget>[
-                SummaryWidget(
-                  income: model.incomeSum,
-                  expense: model.expenseSum),
-                buildList(model.transactions, model)
-              ],
-            ),
-            model.isCollapsed
-                ? buildOverlayPicker(model.isCollapsed, model, context)
-                : Container(),
-          ],
-        ),
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      Text(user.email!),
+                      Text(user.uid),
+                      RoundedButton(
+                          text: "Press me",
+                          press: () => fdb_service.addExpenses(
+                            type: "expense",
+                            user: user,
+                            note: 'Test',
+                            amount: 450,
+                            categoryIndex: 1,
+                            month: 'Jan',
+                            day: "Friday",
+                          )),
+                      SummaryWidget(
+                          income: model.incomeSum, expense: model.expenseSum),
+                      buildList(model.transactions, model)
+                    ],
+                  ),
+                  model.isCollapsed
+                      ? buildOverlayPicker(model.isCollapsed, model, context)
+                      : Container(),
+                ],
+              ),
       ),
-
     );
   }
 
@@ -69,4 +88,3 @@ class HomeView extends StatelessWidget {
         : TransactionsListView(transactions, model);
   }
 }
-
